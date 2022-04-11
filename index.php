88758c6922eaf1b9d958a0b0b1189833e4e12535
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-04-11 17:45:55 +0200

    provo a formattare bene ma non mi si visualizza piu nulla

--- index.php
+++ index.php
@@ -9,51 +9,49 @@
 <body>
     <?php 
         $question_arr = [
-            // -------------------------------------------------------------------QUESTION KEY--------------------------------------------------------------->
-            "<h1> Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?</h1>" => 
+            // --------------------------------------------QUESTIONKEY------------------------------>
+            "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?" => 
             // -------------------------------------------------------------------QUESTION KEY--------------------------------------------------------------->
             // -------------------------------------------------------------------ANSWER--------------------------------------------------------------------->
-            "<p> La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</p>
+            " La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
 
-            <p> Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.</p>
+             Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
 
-            <p> Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.</p>
+             Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.
             
-            <p>Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.</p>",
+            Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.",
             // -------------------------------------------------------------------ANSWER--------------------------------------------------------------------->
 
             // -------------------------------------------------------------------QUESTION KEY--------------------------------------------------------------->
             "<h1>Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?</h1>" =>
             // -------------------------------------------------------------------QUESTION KEY--------------------------------------------------------------->
             // -------------------------------------------------------------------ANSWER--------------------------------------------------------------------->
-            "<p> Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</p>
+            " Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
             
-            <p> Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</p>
+             Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
 
-            <p>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.</p>
+            Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
 
-            <p>Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</p>",
+            Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
             // -------------------------------------------------------------------ANSWER--------------------------------------------------------------------->
 
 
             // -------------------------------------------------------------------ANSWER ARRAY--------------------------------------------------------------->
-             "<h1>Perché il mio account è associato a un paese?</h1>" => [
+             "Perché il mio account è associato a un paese?" => [
             
             // -------------------------------------------------------------------SUB-ANSWER ARRAY----------------------------------------------------------->
-             '<p> Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose: </p>' => [
-                '<ul><li> La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</li>' => [
+             'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose: ' => [
+                'La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:' => [
             // -------------------------------------------------------------------SUB-SUB ANSWER ARRAY ------------------------------------------------------>
-                       '<ul>
-                            <li> Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>',
-                            '<li> Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
-                        </ul>',
+            'Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.',
+            'Google LLC, con sede negli Stati Uniti, per il resto del mondo.',
             // -------------------------------------------------------------------SUB-SUB ANSWER ARRAY ------------------------------------------------------>
             ],
             // -------------------------------------------------------------------SUB ANSWER  --------------------------------------------------------------->
 
-                    '<li> La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali. </li>
-                </ul>',
-                '<p> Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>',
+                'La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+                ',
+                'Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.',
             // -------------------------------------------------------------------SUB ANSWER  --------------------------------------------------------------->
 
             // -------------------------------------------------------------------SUB-SUB ANSWER ARRAY ------------------------------------------------------>
@@ -84,25 +82,47 @@
             // -------------------------------------------------------------------ANSWER--------------------------------------------------------------------->
   
         ];
-
+        ?>
+        <?php 
         foreach ($question_arr as $question_key => $answer) {
-            // foreach ($)
-            echo $question_key . $answer;       
-            foreach ($answer as $sub_key => $sub_answer) {
-                echo $sub_key . $sub_answer; 
-                // var_dump($sub_answer);
-                foreach ($sub_answer as $sub_sub_key=> $sub_sub_answer) {
-                    echo $sub_sub_key . $sub_sub_answer;
-                };
-                foreach ($sub_sub_answer as $sub_sub_value => $sub_sub_sub) {
-                    echo $sub_sub_value . $sub_sub_sub;
-                }; 
+            // foreach ($)?>
+            <h1>
+                <?php echo $question_key; ?>
+            </h1>
+            <p>
+                <?php echo  $answer;?>
+            </p>
+        <?php            
+            foreach ($answer as $sub_key => $sub_answer) { ?>
+            <p>
+                <?php echo $sub_key;?>
+            </p>
+            <?php  
+                foreach ($sub_answer as $sub_sub_key=> $sub_sub_answer) {?>
+                 <ol>
+                    <li>
+                        <?php echo $sub_sub_key;?>
+                        <ol type="a">
+                            <?php foreach ($sub_sub_answer as $sub_sub_value => $sub_sub_sub) {?>
+                                <li>
+                                    <?php echo $sub_sub_value . $sub_sub_sub;?>
+                                </li>
+                            };
+                        </ol>
+                    </li>
+                    <li>
+                        <?php echo $sub_sub_answer; ?>
+                    </li>
+                </ol>
                 
+                    <!-- // echo $sub_sub_key . $sub_sub_answer; -->
+                
+               
+                };
+
             };
                 
-
-            // foreach()
-        }
-    ?>
-</body>
+        };
+    
+    </body>
 </html>
