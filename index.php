9e017a0681e879571436398f5132a9eddf0cc2a6
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-04-11 15:51:14 +0200

    Impostazione database

--- index.php
+++ index.php
@@ -74,9 +74,9 @@
              '<p> In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l\'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l\'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell\'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell\'URL referrer potrebbero essere disponibili mediante Google Analytics o un\'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all\'esatte parole chiave che hanno determinato il clic su un annuncio.</p>',    
         ];
 
-        foreach ($question_arr as $question => $answer) {
+        foreach ($question_arr as $key_question => $answer) {
             // foreach ($)
-            echo $question . $answer;
+            echo $key_question . $answer;
             foreach ($answer as $sub_question => $sub_answer) {
                 echo $sub_question . $sub_answer; 
             };
